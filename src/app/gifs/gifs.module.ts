c780c03574e0ca6b07a6d275f1b2176b0ee880e2
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-10 19:28:58 -0300

    Creo el card-list.component
    	new file:   src/app/gifs/components/card-list/card-list.component.css
    	new file:   src/app/gifs/components/card-list/card-list.component.html
    	new file:   src/app/gifs/components/card-list/card-list.component.spec.ts
    	new file:   src/app/gifs/components/card-list/card-list.component.ts
    	modified:   src/app/gifs/gifs.module.ts
    	modified:   src/app/gifs/pages/home/home-page.component.html

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    HomePageComponent
+    HomePageComponent,
+    CardListComponent
   ]
 })
 export class GifsModule { }
