fbba2dc7962ecb360d24c204b77b5d8ff85f4f84
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-10 19:33:49 -0300

    envío el tag a buscar al servicio.

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor( private gifsService: GifsService ) {}
+
+  searchTag():void {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag( newTag );
+
+    this.tagInput.nativeElement.value = '';
+
+  }
 
 }
