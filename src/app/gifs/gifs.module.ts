e268b5daac6c5c497842dda5e7c48f06c4cc9f7e
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-12 15:54:43 -0300

    Creo el GifsCardComponent y lo utilizo para mostrar las imágenes.

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    CardListComponent
+    CardListComponent,
+    GifsCardComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     HomePageComponent,
-    CardListComponent
+    CardListComponent,
+    GifsCardComponent
   ]
 })
 export class GifsModule { }
