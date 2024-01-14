16aff96bde61e0c193bb5017f34aaa637ac35d77
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-13 21:56:54 -0300

    Coloco un botón para descargar el gif.

--- src/app/gifs/components/gifs-card/gifs-card.component.ts
+++ src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ClipboardService } from 'ngx-clipboard';
+import { HttpClient } from '@angular/common/http';
 
 import { Gif } from '../../interfaces/gifs.interfaces';
 
@@ -12,7 +13,8 @@ import { Gif } from '../../interfaces/gifs.interfaces';
 export class GifsCardComponent implements OnInit {
 
   constructor(
-    private clipboardApi: ClipboardService
+    private clipboardApi: ClipboardService,
+    private http: HttpClient
   ) { }
 
   ngOnInit(): void {
@@ -26,4 +28,17 @@ export class GifsCardComponent implements OnInit {
   copyClipboard( url: string ): void {
     this.clipboardApi.copyFromContent(url);
   }
+
+  downloadFile(url: string, filename: string):void {
+    this.http.get(url, { responseType: 'blob' }).subscribe(response => {
+      const blob = new Blob([response], { type: response.type });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = filename + '.gif';
+      a.click();
+      window.URL.revokeObjectURL(url);
+    });
+  }
+
 }
